9cb6b2496a7606935404418604bb018d2079f32d
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-08-20 01:28:03 +0530

    created auth middleware and update delete functions

--- backend/routes/patientrouter.js
+++ backend/routes/patientrouter.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
-const { patientsignup, patientsignin } = require('../controllers/patientcontroller.js')
+const patientauth = require('../middleware/patientauth');
+const { patientsignup, patientsignin, updatePatient, deletePatient } = require('../controllers/patientcontroller.js')
 
 //patient sign up
 router.post('/patientsignup', patientsignup);
 
 //patient sign in
 router.post('/patientsignin', patientsignin);
+
+//patient update profile
+router.put('/updatepatient', patientauth, updatePatient);
+
+//patient delete profile
+router.delete('/deletpatient', patientauth, deletePatient);
 
 module.exports = router;
