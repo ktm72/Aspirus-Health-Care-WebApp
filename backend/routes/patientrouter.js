430be74bf8d91f01c8ac145ab5b202bd081ea1e8
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-08-20 15:09:26 +0530

    add forgot and reset pw to patientroutes

--- backend/routes/patientrouter.js
+++ backend/routes/patientrouter.js
@@ -1,17 +1,25 @@
 const router = require("express").Router();
 const patientauth = require('../middleware/patientauth');
-const { patientsignup, patientsignin, updatePatient, deletePatient } = require('../controllers/patientcontroller.js')
+const { patientsignup, patientsignin, updatePatient, deletePatient} = require('../controllers/patientcontroller.js');
+const { forgotPassword, resetPassword } = require('../controllers/patientcontroller.js')
 
 //patient sign up
-router.post('/patientsignup', patientsignup);
+router.post('/signup', patientsignup);
 
 //patient sign in
-router.post('/patientsignin', patientsignin);
+router.post('/signin', patientsignin);
 
 //patient update profile
-router.put('/updatepatient', patientauth, updatePatient);
+router.put('/updateprofile', patientauth, updatePatient);
 
 //patient delete profile
-router.delete('/deletpatient', patientauth, deletePatient);
+router.delete('/deleteprofile', patientauth, deletePatient);
+
+//patient forgotPassword
+router.post('/forgotpassword', forgotPassword);
+
+//patient resetPassword
+router.post('/resetpassword', resetPassword);
+
 
 module.exports = router;
