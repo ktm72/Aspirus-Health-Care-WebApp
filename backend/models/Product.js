a104aed5e0e6765521f688875b6fad1310e8ec7f
Tharindu Dasun <tharindudasun0@gmail.com>
2021-08-21 15:00:34 +0530

    Completed the product model

---
+++ backend/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+ 
+const ProductSchema = new Schema({      
+    name : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    type : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    image :{
+        type : String,
+        required : false
+    }
+    
+});
+
+const Product = mongoose.model("product",ProductSchema) 
+module.exports = Product
