07a07566873e32ca7cefc8df33a5af86a135ea08
Pasan Withanage <pasanwithanage530@gmail.com>
2021-08-22 12:40:24 +0530

    Create appointment model

---
+++ backend/models/Appointment.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AppointmentSchema = new Schema({
+    patientID: {
+        type: Schema.Types.ObjectId,
+        ref:'patient',
+        require: true
+    },
+
+    doctorID: {
+        type: Schema.Types.ObjectId,
+        ref:'doctor',
+        require: true
+    },
+    
+    time: {
+        type: String,
+        require: true
+    },
+
+    date: {
+        type: String,
+        require: true
+    }
+
+});
+
+const Appointment = mongoose.model("Appointment",AppointmentSchema)
+
+module.exports = Appointment
