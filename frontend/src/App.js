1c35a2b3be1d38e017db09ada9a3d7d56ba3ea07
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-08-30 20:39:18 +0530

    added patient management routes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import { BrowserRouter as Router,Route } from 'react-router-dom';
 import './App.css';
-import Login from './components/PatientManagement/Login/Login';
+import PatientSignIn from './components/PatientManagement/SignIn/SignIn';
+import PatientSignUp from './components/PatientManagement/SignUp/SignUp';
+import ForgotPassword from './components/PatientManagement/ForgotPassword/ForgotPassword';
+import ResetPassword from './components/PatientManagement/ResetPassword/ResetPassword';
+import Profile from './components/PatientManagement/Profile/Profile';
+import UpdateProfile from './components/PatientManagement/UpdateProfile/UpdateProfile'
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <Route path="/patient/login" exact component={Login}/>
+          <Route path="/patient/signin" exact component={PatientSignIn} />
+          <Route path="/patient/signup" exact component={PatientSignUp} />
+          <Route path="/patient/forgotpassword" exact component={ForgotPassword} />
+          <Route path="/patient/resetpassword" exact component={ResetPassword} />
+          <Route path="/patient/profile" exact component={Profile} />
+          <Route path="/patient/update/profile" exact component={UpdateProfile} />
         </div>
       </Router>
     </div>
