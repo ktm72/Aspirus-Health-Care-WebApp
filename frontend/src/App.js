d579e4b65dda4319ed8be25342847b6ec9af7531
Joshua Amarasinghe <joshuaamarasinghe@gmail.com>
2021-09-01 23:04:17 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter as Router,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/PatientManagement/Login/Login';
+import PrescriptionHistory from './components/PrescriptionManagement/History/History';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <Route path="/patient/login" exact component={Login}/>
+          <Route path="/patient/login" exact component={Login} />
+          <Route path="/prescription/history" exact component={PrescriptionHistory} />
         </div>
       </Router>
     </div>
