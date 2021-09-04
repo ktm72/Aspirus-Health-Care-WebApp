c98be1225981abb962b5f9eb52789790543148a5
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-09-04 19:06:57 +0530

    sign up frontend completed without image handling

---
+++ frontend/src/components/PatientManagement/SignUp/SignUp.js
@@ -0,0 +1,234 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import Button from "@material-ui/core/Button";
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import axios from 'axios';
+import './SignUp.css';
+
+function SignUp() {
+    const [firstname,setFirstName] = useState("");
+    const [lastname,setLastName] = useState("");
+    const [email,setEmail] = useState("");
+    const [phone,setPhone] = useState("");
+    const [dob,setDob] = useState();
+    const [nic,setNIC] = useState("");
+    const [address,setAddress] = useState("");
+    const [gender,setGender] = useState();
+    const [password,setPassword] = useState("");
+    const [confirmpassword,setConfirmPassword] = useState("");
+    const [showPassword, setShowPassword] = useState()
+
+    //show hide password
+    function handleShowPassword(){
+        setShowPassword((prevShowPassword) => !prevShowPassword)
+    }
+
+    //add new item
+    async function Register(event){
+
+        event.preventDefault();
+
+        if(password === confirmpassword){
+            //object with data need to be added
+            const newPatient = new FormData();
+            newPatient.append("firstname", firstname);
+            newPatient.append("lastname", lastname);
+            newPatient.append("email", email);
+            newPatient.append("phone", phone);
+            newPatient.append("dob", dob);
+            newPatient.append("nic", nic);
+            newPatient.append("address", address);
+            newPatient.append("gender", gender);
+            newPatient.append("password", password);
+
+            try {
+                await axios.post("http://localhost:8070/patient/signup",newPatient);
+                alert("Registration Successful")
+                setFirstName("");
+                setLastName("");
+                setEmail("");
+                setPhone("");
+                setDob();
+                setNIC("");
+                setAddress("");
+                setGender();
+                setPassword("");
+            } catch (error) {
+                if(error.response.status === 409){
+                    alert("User with this email address already exists")
+                }
+                else{
+                    alert("Registration Failed")
+                }
+            }
+        }else{
+            alert("Passwords don't match");
+        }        
+    }
+
+    return (
+            <div className="container" >
+                <div className="card-form">
+                    <form onSubmit={Register} class="boxSignUp">
+                        <h1 className="form-h1">Sign Up</h1>
+                        <br></br>
+                        <div class="row">
+                            <div class="col-md-6 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput
+                                        type="text" id="firstname" placeholder="First Name" required fullWidth
+                                        onChange={(event)=> {setFirstName(event.target.value)}}
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput  
+                                        type="text" id="lastname" placeholder="Last Name" required fullWidth
+                                        onChange={(event)=> {setLastName(event.target.value)}}
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-4 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput 
+                                        type="date" id="dob" required fullWidth
+                                        onChange={(event)=> {setDob(event.target.value)}}
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-8 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput  
+                                        type="email" id="email" placeholder="Email" required fullWidth
+                                        onChange={(event)=> {setEmail(event.target.value)}}
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput 
+                                        type="tel" id="phone" placeholder="phone" required fullWidth
+                                        onChange={(event)=> {setPhone(event.target.value)}}
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput 
+                                        type="text" id="nic" placeholder="NIC" required fullWidth
+                                        onChange={(event)=> {setNIC(event.target.value)}}
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput 
+                                        type="text" id="address" placeholder="Address" required fullWidth
+                                        onChange={(event)=> {setAddress(event.target.value)}}
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <div class="form-group">
+                                    <label>Gender</label> &nbsp;
+                                    <div class="form-check form-check-inline">
+                                        <input 
+                                            class="form-check-input" type="radio" name="gender" id="male" value="Male" required
+                                            onChange={(event)=> {setGender(event.target.value)}}
+                                        />
+                                        <label class="form-check-label" for="male">Male</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input 
+                                            class="form-check-input" type="radio" name="gender" id="female" value="Female" required
+                                            onChange={(event)=> {setGender(event.target.value)}}
+                                        />
+                                        <label class="form-check-label" for="female">Female</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <div class="form-group">
+                                    <label For="profilepic">
+                                        <input
+                                            style={{ display: 'none' }}
+                                            id="profilepic"
+                                            name="profilepic"
+                                            type="file"
+                                        />
+
+                                        <Button color="primary" variant="contained" component="span">
+                                            <AddAPhotoIcon/> &nbsp; Upload Profile Picture
+                                        </Button>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <OutlinedInput 
+                                        type={showPassword ? "text" : "password"}
+                                        id="password" placeholder="Password" required fullWidth
+                                        onChange={(event)=> {setPassword(event.target.value)}}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                              <IconButton onClick={handleShowPassword}>
+                                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                              </IconButton>
+                                            </InputAdornment>
+                                        }
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <div class="form-group">
+                                    <OutlinedInput 
+                                        type={showPassword ? "text" : "password"}
+                                        id="confirmpassword" placeholder="Confirm Password" required fullWidth
+                                        onChange={(event)=> {setConfirmPassword(event.target.value)}}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                              <IconButton onClick={handleShowPassword}>
+                                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                              </IconButton>
+                                            </InputAdornment>
+                                        }
+                                        inputProps={{style: {padding: 12}}}
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <div class="form-group">
+                                    <input id="terms" type="checkbox" required/>
+                                    <label for="terms">I agree to the <Link to="/terms">Terms and Conditions</Link>.</label>
+                                </div>
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <div class="form-group">
+                                    <input className="form-submit-btn" type="submit" value="Sign Up" />
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <p>Already have an account? <Link to="/patient/signin">Sign In</Link></p>
+                    </form>             
+                </div>                    
+            </div>
+    )
+}
+
+export default SignUp
