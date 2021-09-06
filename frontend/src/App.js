1cf90b3ca5abbe01d4f1f36cfdab7ebdf978a0d5
Joshua Amarasinghe <joshuaamarasinghe@gmail.com>
2021-09-06 15:25:55 +0530

    Updated App.js ( Add Prescription)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/PatientManagement/Login/Login';
+import Add from './components/PrescriptionManagement/Add/Add';
 import History from './components/PrescriptionManagement/History/History';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <Router>
         <div>
           <Route path="/patient/login" exact component={Login} />
-          <Route path="/prescription/history" exact component={History} />
+          <Route path="/prescription/history/" exact component={History} />
+          <Route path="/prescription/add" exact component={Add} />
         </div>
       </Router>
     </div>
