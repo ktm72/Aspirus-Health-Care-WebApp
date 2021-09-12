49b4b5b2e623893d49f72347a0d1141470b66568
OnaraPathiraja014 <onarap2000@gmail.com>
2021-09-12 21:47:04 +0530

    Added doctor route to app.js file.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router,Route } from 'react-router-dom';
 import './App.css';
-import Login from './components/PatientManagement/Login/Login';
+import DoctorLogin from './components/DoctorManagement/DoctorLogin/DoctorLogin';
+import DoctorSignUp from './components/DoctorManagement/DoctorSignUp/DoctorSignUp';
+import DoctorProfile from './components/DoctorManagement/DoctorProfile/DoctorProfile';
+import DoctorUpdate from './components/DoctorManagement/DoctorUpdateProfile/DoctorUpdate';
+import DoctorPrivateRoute from './Routes/DoctorPrivateRoute';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Route path="/patient/login" exact component={Login}/>
+        <div> 
+          <Route path="/doctor/signin" exact component={DoctorLogin}/>  
+          <Route path="/doctor/signup" exact component={DoctorSignUp}/>  
+          <DoctorPrivateRoute path="/doctor/profile" exact component={DoctorProfile}/>
+          <DoctorPrivateRoute path="/doctor/update/:id" exact component={DoctorUpdate}/> 
         </div>
       </Router>
     </div>
