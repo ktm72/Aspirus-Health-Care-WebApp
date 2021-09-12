bc1e38dc32ae722a06882d0b533973da0f66a86f
Tharindu Dasun <tharindudasun0@gmail.com>
2021-09-12 16:12:38 +0530

    Added Pharmacy frontend routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router,Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/PatientManagement/Login/Login';
+import AddProducts from './components/PharmacyManagement/AddProduct/AddProducts'
+import Items from './components/PharmacyManagement/Items/Items'
+import SingleItem from './components/PharmacyManagement/SingleItem/SingleItem'
+import UpdateProduct from './components/PharmacyManagement/UpdateProduct/UpdateProduct'
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Router>
         <div>
           <Route path="/patient/login" exact component={Login}/>
+          <Route path="/pharmacy/addProduct" exact component={AddProducts}/>
+          <Route path="/pharmacy/items" exact component={Items}/>
+          <Route path="/pharmacy/item/:id" exact component={SingleItem}/>
+          <Route path="/pharmacy/item/update/:id" exact component={UpdateProduct}/>
         </div>
       </Router>
     </div>
