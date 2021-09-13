aa23016b4ab56786965bcea1d2c110c2f791d652
sathinka <sathinkawijesingha123@gmail.com>
2021-09-13 17:55:21 +0530

    added the review and payment routes to the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router,Route } from 'react-router-dom';
 import './App.css';
-import Login from './components/PatientManagement/Login/Login';
+import AddPayment from './components/PaymentManagement/AddPayment/AddPayment';
+import AllPayments from './components/PaymentManagement/AllPayments/AllPayments';
+import CreateReview from './components/ReviewManagement/CreateReview/CreateReview';
+import DisplayReview from './components/ReviewManagement/DisplayReview/DisplayReview';
+import updateReview from './components/ReviewManagement/UpdateReview/UpdateReview';
 
 function App() {
   return (
     <div className="App">
+     
       <Router>
         <div>
-          <Route path="/patient/login" exact component={Login}/>
+          <Route path= "/patient/payment" exact component= {AddPayment}/>
+          <Route path="/patient/payment/:patientID" exact component = {AllPayments}/>
+          <Route path="/patient/review" exact component={CreateReview}/>
+          <Route path="/patient/review/:patientID" exact component = {DisplayReview}/>
+          <Route path="/patient/review/update/:id" exact component = {updateReview}/>
+         
         </div>
       </Router>
     </div>
