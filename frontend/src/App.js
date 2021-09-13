558687d9b4ac1fd5e8e430fa043108e145ce3464
Ashen-Ravisara <ashen2ravisara@gmail.com>
2021-09-13 21:49:10 +0530

    create the routes for cart management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router,Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/PatientManagement/Login/Login';
+import Cart from './components/CartManagement/Cart';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <div>
           <Route path="/patient/login" exact component={Login}/>
+          <Route path="/cart/:id/:type" exact component={Cart}/>
         </div>
       </Router>
     </div>
