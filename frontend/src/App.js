504fb35bde1921950c205ad1ed3c938d4ada9e5e
Ashen-Ravisara <ashen2ravisara@gmail.com>
2021-10-06 08:42:35 +0530

    desigen the interface of the view and add appointments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,6 +28,8 @@ import AllPayments from './components/PaymentManagement/AllPayments/AllPayments'
 import CreateReview from './components/ReviewManagement/CreateReview/CreateReview';
 import DisplayReview from './components/ReviewManagement/DisplayReview/DisplayReview';
 import updateReview from './components/ReviewManagement/UpdateReview/UpdateReview';
+import AddAppointment from './components/AppointmentManagement/AddAppointment';
+import ViewAppointment from './components/AppointmentManagement/ViewAppointment';
 import Footer from './components/Footer/Footer';
 import Homepage from './components/Home/Homepage';
 
@@ -62,6 +64,9 @@ function App() {
             <PatientPrivateRoute path="/patient/review" exact component={CreateReview}/>
             <PatientPrivateRoute path="/patient/review/:patientID" exact component = {DisplayReview}/>
             <PatientPrivateRoute path="/patient/review/update/:id" exact component = {updateReview}/>
+                       
+            <Route path="/cart/AddAppointment" exact component={AddAppointment}/>
+            <Route path="/cart/ViewAppointment" exact component={ViewAppointment}/>
             <Footer/>
         </div>
       </Router>
