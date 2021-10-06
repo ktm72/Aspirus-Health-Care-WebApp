a9bb5befb9d56756ea7d85073cfeddbb4bf3f07b
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-10-06 22:16:29 +0530

    appointment component for profie

--- frontend/src/components/AppointmentManagement/ProfileAppointments.js
+++ frontend/src/components/AppointmentManagement/ProfileAppointments.js
@@ -1,35 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useHistory} from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
+import { green } from '@material-ui/core/colors';
 
 function ProfileAppointments() {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+    const [appointments, setAppointments] = useState([])
+    const [isDoctor,setIsDoctor]=useState(false)
+    const history = useHistory()
+
+    useEffect(() => { 
+      if(localStorage.getItem("doctorAuthToken")){
+        setIsDoctor(true)
+      }else{
+        setIsDoctor(false)
+      }
+
+      async function getAppointments() {
+        await axios.get(`http://localhost:8070/appointment/${user._id}`).then((res) => {
+          setAppointments(res.data.result)
+        }).catch((error) => {
+          alert("Failed to fetch the appointment history")
+        })
+      }
+      getAppointments();
+
+    }, [user])
+
+    function appointmentHistory(id) {
+      history.push(`/appointment/${id}`)
+    }
+
+    
+    function videoConference(id) {
+      history.push(`/video/${id}`)
+    }
+
+
     return (
-      <div className="container"  >
         <div className="blue-table ">
           <div className="blue-table, box-view-prescription">
             <table>
               <thead >
                 <tr>
+                  <th style={{ textAlign: 'center' }}>{isDoctor ? "Name of the Patient" : "Name of the Doctor" }</th>
                   <th style={{ textAlign: 'center' }}>Date</th>
-                  <th style={{ textAlign: 'center' }}>Name of the patient</th>
                   <th style={{ textAlign: 'center' }}>time</th>
+                  <th style={{ textAlign: 'center' }}>Conference</th>
                 </tr>
               </thead>
               <tbody style={{ textAlign: 'center' }}>
-                  <tr>
-                    <td>
-                      YYYY-MM-DD
+                {appointments.slice(0,4).map((Appointment, key) => (
+                  <tr key={key} >
+                    <td onClick={() => appointmentHistory(Appointment.patientID._id)}>
+                    {
+                      isDoctor ?
+                        Appointment.patientID.firstname + " " + Appointment.patientID.lastname
+                      :
+                        Appointment.doctorID.title + " " + Appointment.doctorID.name
+                    }
+                    </td>
+                    <td onClick={() => appointmentHistory(Appointment.patientID._id)}>
+                      {Appointment.date}
+                    </td>
+                    <td onClick={() => appointmentHistory(Appointment.patientID._id)}>
+                      {Appointment.time}
                     </td>
                     <td>
-                      Mr.John Doe
-                    </td>
-                    <td>
-                      HH:MM
+                      <IconButton onClick={() => videoConference(Appointment._id)}>
+                        <VideoCameraFrontIcon style={{ color: green[500] }} fontSize="large"  ></VideoCameraFrontIcon>
+                      </IconButton>
                     </td>
                   </tr>
+                ))}
               </tbody>
             </table>
           </div>
         </div>
-      </div>
     )
 }
 
