210189d8841f500451de419ba8ba094a9de4a9b7
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-10-07 17:31:08 +0530

    indentation corrected

--- frontend/src/components/PharmacyManagement/Items/Items.js
+++ frontend/src/components/PharmacyManagement/Items/Items.js
@@ -50,41 +50,43 @@ function ProductItem() {
   function view(id){
     history.push(`/pharmacy/item/${id}`)
   }
+  
   function addProduct(){
     history.push(`/pharmacy/addProduct`)
   }
-    return (
-        <div className="container productGrid" > 
-          {isAdmin && 
-            <Button  className="mx-2 productBtn" style={{backgroundColor:blue[400],color:'white'}} onClick={()=>addProduct()}>
-            Add Product <AddIcon/>
-            </Button> 
-          }
-          {products.map((Product,key)=>( 
-                <div key={key}> 
-                    <div className="productCard">
-                        <div className="imgBx">
-                            <img  src="/images/s.jpg" alt="product" className="itemProduct"/>
-                        </div>
-                        <div className="p-3">
-                            <h7>{Product.name}</h7>
-                            <h6>Rs.{Product.price}.00</h6>
-                            <div align="right">
-                              <span> 
-                                  <button className="productBtn" style={{backgroundColor:orange[600]}}
-                                    onClick={()=>AddToCart(Product._id, user._id, Product.price)}> 
-                                    <ShoppingCartIcon/> 
-                                  </button>
-                                  &nbsp;&nbsp;&nbsp;
-                                  <button className="productBtn" style={{backgroundColor:red[400]}} onClick={()=>view(Product._id)}> View Item </button>
-                              </span> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-          ))}
+
+  return (
+    <div className="container productGrid" > 
+      {isAdmin && 
+        <Button  className="mx-2 productBtn" style={{backgroundColor:blue[400],color:'white'}} onClick={()=>addProduct()}>
+        Add Product <AddIcon/>
+        </Button> 
+      }
+      {products.map((Product,key)=>( 
+        <div key={key}> 
+          <div className="productCard">
+            <div className="imgBx">
+              <img  src="/images/s.jpg" alt="product" className="itemProduct"/>
+            </div>
+            <div className="p-3">
+              <h7>{Product.name}</h7>
+              <h6>Rs.{Product.price}.00</h6>
+              <div align="right">
+                <span> 
+                  <button className="productBtn" style={{backgroundColor:orange[600]}}
+                    onClick={()=>AddToCart(Product._id, user._id, Product.price)}> 
+                    <ShoppingCartIcon/> 
+                  </button>
+                  &nbsp;&nbsp;&nbsp;
+                  <button className="productBtn" style={{backgroundColor:red[400]}} onClick={()=>view(Product._id)}> View Item </button>
+                </span> 
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default ProductItem
