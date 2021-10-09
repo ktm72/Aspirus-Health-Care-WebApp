679856a813aa6718ead2518c33666c465773fce2
Ashen-Ravisara <ashen2ravisara@gmail.com>
2021-10-10 00:27:22 +0530

    add intro and review home componants and mobile app download link

--- frontend/src/components/Home/Homepage.js
+++ frontend/src/components/Home/Homepage.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Covid from './Covid/Covid'
+import Intro from './Intro/Intro'
+import ReviewsHome from './Review/ReviewsHome'
 
 function Homepage() {
     return (
-        <div>
-            <Covid />
+        <div className="container">
+            <Covid/>
+            <Intro/>               
+            <div className="row">
+                <div className="col-8">
+                    <img src="../images/appStore.png" alt="download from store" width="200px" style={{position:'absolute', marginTop:'175px', marginLeft:'50px'}}/>
+                    <img src="../images/downloadApp.png" width="800px" alt="appImage" style={{ borderRadius: 400/25, paddingLeft:"10px"}}/>
+                </div>
+                <div className="col-4">
+                    <ReviewsHome/>
+                </div>                    
+            </div>
         </div>
     )
 }
