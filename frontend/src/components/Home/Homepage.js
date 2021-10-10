c3699712e4e931ad7ae16bab1406d641a3f91dde
Tharindu Dasun <tharindudasun0@gmail.com>
2021-10-10 21:44:44 +0530

    created the home page pharmacy

--- frontend/src/components/Home/Homepage.js
+++ frontend/src/components/Home/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Covid from './Covid/Covid'
+import PharmacyHome from './PharmacyHome'
 
 function Homepage() {
     return (
-        <div>
+        <div className="container">
             <Covid />
+            <PharmacyHome/>
         </div>
     )
 }
