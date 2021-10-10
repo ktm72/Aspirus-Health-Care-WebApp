c7cadcb0fc44e0105b5a52302f40d738c714d99d
sathinka <sathinkawijesingha123@gmail.com>
2021-10-11 01:20:32 +0530

    add validations

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -21,9 +21,16 @@ const PaymentSchema=new Schema({
         match:/^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/,
        
     },
+    cvv:{
+        type:String,
+        required: true,
+        //validate cvv
+        match:/^[0-9]{3,4}$/,
+
+    },
     date:{
         type:String,
-        required:true
+        required:true, 
     }
 
 })
